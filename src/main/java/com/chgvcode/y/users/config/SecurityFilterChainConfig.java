81a60a22f288f41fe8b8cb47f882350a5d6967dc
Christian G <christiangv99@gmail.com>
2025-12-08 01:14:24 +0000

    Rename security filter chain configuration class

--- src/main/java/com/chgvcode/y/users/config/SecurityFilterChainConfig.java
+++ src/main/java/com/chgvcode/y/users/config/SecurityFilterChainConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityFilterChainConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http

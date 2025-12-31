fdc3fee99c8c7e3e0f8c427ab492dbf2f2c5d9d7
Christian G <christiangv99@gmail.com>
2025-12-31 05:34:28 +0000

    Update user response dto

--- src/main/java/com/chgvcode/y/users/dto/UserResponse.java
+++ src/main/java/com/chgvcode/y/users/dto/UserResponse.java
@@ -1,10 +1,11 @@
 package com.chgvcode.y.users.dto;
 
 import java.time.Instant;
+import java.util.UUID;
 
 public record UserResponse(
     Long id,
+    UUID uuid,
     String username,
-    String password,
     Instant createdAt
 ) {}

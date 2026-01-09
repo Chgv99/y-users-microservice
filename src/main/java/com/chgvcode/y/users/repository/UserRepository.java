f1de0aa07d61c59face8ae9c3467c358f4b39b31
Christian G <christiangv99@gmail.com>
2026-01-09 19:27:16 +0000

    Update user repository

--- src/main/java/com/chgvcode/y/users/repository/UserRepository.java
+++ src/main/java/com/chgvcode/y/users/repository/UserRepository.java
@@ -1,15 +1,17 @@
 package com.chgvcode.y.users.repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chgvcode.y.users.model.UserEntity;
-import java.util.List;
-import java.util.UUID;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    public Optional<UserEntity> findByUuid(UUID uuid);
     public Optional<UserEntity> findByUsername(String username);
+    public List<UserEntity> findByUsernameIn(List<String> usernames);
     public List<UserEntity> findByUuidIn(List<UUID> uuids);
 }

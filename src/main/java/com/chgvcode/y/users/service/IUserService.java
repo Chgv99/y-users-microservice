066519ea53d45c0a44a6d5657ebd9aed63d6f8bd
Christian G <christiangv99@gmail.com>
2026-01-09 19:26:49 +0000

    Update user service

--- src/main/java/com/chgvcode/y/users/service/IUserService.java
+++ src/main/java/com/chgvcode/y/users/service/IUserService.java
@@ -3,10 +3,19 @@ package com.chgvcode.y.users.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.chgvcode.y.users.dto.UserResponse;
 import com.chgvcode.y.users.model.UserEntity;
 
 public interface IUserService {
-    public List<UserEntity> getUsers();
-    public List<UserEntity> getUsersByUuids(List<UUID> uuids);
-    public UserEntity createUser(String username, String password);
+    public UserResponse getUserByUsername(String username);
+    public List<UserResponse> getUserListByUsernames(List<String> usernames);
+    public UserEntity getUserEntityByUsername(String username);
+    public UserResponse getUserByUuid(UUID uuid);
+    public List<UserResponse> getUsersByUuids(List<UUID> uuids);
+    public Page<UserResponse> getUsers(Pageable pageable);
+    public UserResponse createUser(String username, String password);
+    public UserEntity createUserEntity(String username, String password);
 }
--- src/main/java/com/chgvcode/y/users/service/UserService.java
+++ src/main/java/com/chgvcode/y/users/service/UserService.java
@@ -3,8 +3,13 @@ package com.chgvcode.y.users.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.chgvcode.y.users.dto.UserResponse;
+import com.chgvcode.y.users.messaging.UserMessageProducer;
 import com.chgvcode.y.users.model.UserEntity;
 import com.chgvcode.y.users.repository.UserRepository;
 
@@ -13,23 +18,58 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
+    
     private final UserRepository userRepository;
+    private final UserMessageProducer userMessageProducer;
 
-    // Needs to map userentity into userresponse
-    public List<UserEntity> getUsers() {
-        return userRepository.findAll();
+    public UserResponse getUserByUsername(String username) {
+        UserEntity userEntity = getUserEntityByUsername(username);
+        return new UserResponse(userEntity.getId(), userEntity.getUuid(), userEntity.getUsername(), userEntity.getCreatedAt());
     }
-    
-    public List<UserEntity> getUsersByUuids(List<UUID> uuids) {
-        return userRepository.findByUuidIn(uuids);
+
+    public List<UserResponse> getUserListByUsernames(List<String> usernames) {
+        List<UserEntity> userEntities = userRepository.findByUsernameIn(usernames);
+        return userEntities.stream()
+                .map(userEntity -> new UserResponse(userEntity.getId(), userEntity.getUuid(), userEntity.getUsername(),
+                        userEntity.getCreatedAt()))
+                .toList();
     }
-    
-    public UserEntity getByUsername(String username) {
+
+    public UserEntity getUserEntityByUsername(String username) {
         return userRepository.findByUsername(username).orElseThrow();
     }
 
-    public UserEntity createUser(String username, String password) {
+    public UserResponse getUserByUuid(UUID uuid) {
+        UserEntity userEntity = userRepository.findByUuid(uuid).orElseThrow();
+        return new UserResponse(userEntity.getId(), userEntity.getUuid(), userEntity.getUsername(), userEntity.getCreatedAt());
+    }
+
+    public List<UserResponse> getUsersByUuids(List<UUID> uuids) {
+        List<UserEntity> userEntities = userRepository.findByUuidIn(uuids);
+        return userEntities.stream()
+                .map(userEntity -> new UserResponse(userEntity.getId(), userEntity.getUuid(), userEntity.getUsername(),
+                        userEntity.getCreatedAt()))
+                .toList();
+    }
+
+    public Page<UserResponse> getUsers(Pageable pageable) {
+        Page<UserEntity> page = userRepository.findAll(pageable);
+        List<UserResponse> userResponses = page.stream()
+                .map(userEntity -> new UserResponse(userEntity.getId(), userEntity.getUuid(), userEntity.getUsername(),
+                        userEntity.getCreatedAt()))
+                .toList();
+        return new PageImpl<>(userResponses, pageable, page.getTotalElements());
+    }
+
+    public UserResponse createUser(String username, String password) {
+        UserEntity userEntity = createUserEntity(username, password);
+        return new UserResponse(userEntity.getId(), userEntity.getUuid(),
+                userEntity.getUsername(), userEntity.getCreatedAt());
+    }
+
+    public UserEntity createUserEntity(String username, String password) {
         UserEntity user = new UserEntity(username, password);
+        userMessageProducer.sendMessage(user);
         return userRepository.save(user);
     }
 }

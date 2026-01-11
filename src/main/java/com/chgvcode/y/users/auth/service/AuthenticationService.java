905b184741523e8b05006c13e541c3076070b87c
Christian G <christiangv99@gmail.com>
2026-01-11 21:20:59 +0000

    Update authentication service registration

--- src/main/java/com/chgvcode/y/users/auth/service/AuthenticationService.java
+++ src/main/java/com/chgvcode/y/users/auth/service/AuthenticationService.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Service;
 import com.chgvcode.y.users.auth.dto.AuthenticationRequest;
 import com.chgvcode.y.users.auth.dto.AuthenticationResponse;
 import com.chgvcode.y.users.auth.dto.RegisterRequest;
-import com.chgvcode.y.users.messaging.UserMessageProducer;
-import com.chgvcode.y.users.model.UserEntity;
+import com.chgvcode.y.users.auth.dto.RegisterResponse;
+import com.chgvcode.y.users.dto.RegisterUserResponse;
+import com.chgvcode.y.users.dto.UserResponse;
 import com.chgvcode.y.users.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,28 +21,28 @@ public class AuthenticationService implements IAuthenticationService {
 
     private final UserService userService;
 
-    private final UserMessageProducer userMessageProducer;
-
     private final PasswordEncoder passwordEncoder;
-
-    private final IJwtService jwtService;
 
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        UserEntity userEntity = userService.createUserEntity(request.username(), passwordEncoder.encode(request.password()));
+    public RegisterResponse register(RegisterRequest request) {
+        RegisterUserResponse userResponse = userService.createUser(
+            request.username(), 
+            passwordEncoder.encode(request.password()),
+            request.firstName(),
+            request.lastName()
+        );
 
-        String jwt = jwtService.generateToken(userEntity);
-        userMessageProducer.sendUserCreated(userEntity);
-        return new AuthenticationResponse(jwt);
+        String jwt = userService.generateToken(userResponse.uuid());
+        return new RegisterResponse(userResponse.uuid(), userResponse.username(), userResponse.firstName(), userResponse.lastName(), jwt);
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()));
-        UserEntity user = userService.getUserEntityByUsername(request.username());
+        UserResponse userResponse = userService.getUserByUsername(request.username());
 
-        String jwt = jwtService.generateToken(user);
+        String jwt = userService.generateToken(userResponse.uuid());
         return new AuthenticationResponse(jwt);
     }
 }
--- src/main/java/com/chgvcode/y/users/auth/service/IAuthenticationService.java
+++ src/main/java/com/chgvcode/y/users/auth/service/IAuthenticationService.java
@@ -3,8 +3,9 @@ package com.chgvcode.y.users.auth.service;
 import com.chgvcode.y.users.auth.dto.AuthenticationRequest;
 import com.chgvcode.y.users.auth.dto.AuthenticationResponse;
 import com.chgvcode.y.users.auth.dto.RegisterRequest;
+import com.chgvcode.y.users.auth.dto.RegisterResponse;
 
 public interface IAuthenticationService {
-    public AuthenticationResponse register(RegisterRequest request);
+    public RegisterResponse register(RegisterRequest request);
     public AuthenticationResponse authenticate(AuthenticationRequest request);
 }

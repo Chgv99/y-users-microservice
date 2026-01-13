fee7f7586f2c9328aed330468b24ce1839bf8e10
Christian G <christiangv99@gmail.com>
2026-01-13 21:27:56 +0000

    Rename fields with wrong naming convention

--- src/main/java/com/chgvcode/y/users/dto/UserRequest.java
+++ src/main/java/com/chgvcode/y/users/dto/UserRequest.java
@@ -3,6 +3,6 @@ package com.chgvcode.y.users.dto;
 public record UserRequest (
     String username,
     String password,
-    String first_name,
-    String last_name
+    String firstName,
+    String lastName
 ){}

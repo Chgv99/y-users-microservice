1f55f33876a4d75c581a3c2b78cf73abc16aa87c
Christian G <christiangv99@gmail.com>
2026-01-26 22:34:03 +0000

    Implement refresh token in register endpoint

--- src/main/java/com/chgvcode/y/users/auth/controller/AuthenticationController.java
+++ src/main/java/com/chgvcode/y/users/auth/controller/AuthenticationController.java
@@ -1,5 +1,8 @@
 package com.chgvcode.y.users.auth.controller;
 
+import org.apache.hc.core5.http.NotImplementedException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.chgvcode.y.users.auth.dto.AuthenticationRequest;
 import com.chgvcode.y.users.auth.dto.TokenResponse;
+import com.chgvcode.y.users.auth.model.RefreshTokenEntity;
 import com.chgvcode.y.users.auth.dto.RegisterRequest;
 import com.chgvcode.y.users.auth.dto.RegisterResponse;
 import com.chgvcode.y.users.auth.service.IAuthenticationService;
+import com.chgvcode.y.users.auth.service.IRefreshTokenService;
+import com.chgvcode.y.users.auth.service.RefreshCookieFactory;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -22,10 +29,23 @@ public class AuthenticationController {
     
     private final IAuthenticationService authenticationService;
 
+    private final IRefreshTokenService refreshTokenService;
+
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest request, HttpServletResponse response) {
+        RegisterResponse registerResponse = authenticationService.register(request);
+        RefreshTokenEntity refreshToken = refreshTokenService.createRefreshToken(registerResponse.uuid());
+        ResponseCookie cookie = RefreshCookieFactory.create(refreshToken.getToken(), refreshToken.getExpiresAt());
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        return ResponseEntity.ok(registerResponse);
     }
+
+    // TODO
+    // @PostMapping("/refresh")
+    // public ResponseEntity<?> refresh() {
+    //     throw new NotImplementedException();
+    //     return ResponseEntity.ok();
+    // }
     
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
--- src/main/java/com/chgvcode/y/users/auth/service/AuthenticationService.java
+++ src/main/java/com/chgvcode/y/users/auth/service/AuthenticationService.java
@@ -6,9 +6,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.chgvcode.y.users.auth.dto.AuthenticationRequest;
-import com.chgvcode.y.users.auth.dto.TokenResponse;
 import com.chgvcode.y.users.auth.dto.RegisterRequest;
 import com.chgvcode.y.users.auth.dto.RegisterResponse;
+import com.chgvcode.y.users.auth.dto.TokenResponse;
 import com.chgvcode.y.users.dto.RegisterUserResponse;
 import com.chgvcode.y.users.dto.UserResponse;
 import com.chgvcode.y.users.mapper.AuthenticationMapper;
@@ -22,6 +22,8 @@ public class AuthenticationService implements IAuthenticationService {
 
     private final UserService userService;
 
+    private final IJwtService jwtService;
+
     private final PasswordEncoder passwordEncoder;
 
     private final AuthenticationManager authenticationManager;
@@ -30,14 +32,14 @@ public class AuthenticationService implements IAuthenticationService {
 
     public RegisterResponse register(RegisterRequest request) {
         RegisterUserResponse userResponse = userService.createUser(
-            request.username(), 
-            passwordEncoder.encode(request.password()),
-            request.firstName(),
-            request.lastName()
-        );
+                request.username(),
+                passwordEncoder.encode(request.password()),
+                request.firstName(),
+                request.lastName());
 
-        TokenResponse tokenResponse = userService.generateToken(userResponse.uuid());
-        return authenticationMapper.toRegisterResponse(userResponse, tokenResponse);
+        TokenResponse accessToken = jwtService.generateToken(userResponse.uuid().toString(), userResponse.username(),
+                userResponse.role(), userResponse.createdAt());
+        return authenticationMapper.toRegisterResponse(userResponse, accessToken);
     }
 
     public TokenResponse authenticate(AuthenticationRequest request) {
@@ -45,6 +47,7 @@ public class AuthenticationService implements IAuthenticationService {
                 .authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()));
         UserResponse userResponse = userService.getUserByUsername(request.username());
 
-        return userService.generateToken(userResponse.uuid());
+        return jwtService.generateToken(userResponse.uuid().toString(), userResponse.username(), userResponse.role(),
+                userResponse.createdAt());
     }
 }

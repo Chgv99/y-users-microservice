dc0576d443cd885a2fa42afd9bd5fce820ae9b71
Christian G <christiangv99@gmail.com>
2026-01-26 19:34:15 +0000

    Update user responses with role

--- src/main/java/com/chgvcode/y/users/dto/RegisterUserResponse.java
+++ src/main/java/com/chgvcode/y/users/dto/RegisterUserResponse.java
@@ -3,10 +3,13 @@ package com.chgvcode.y.users.dto;
 import java.time.Instant;
 import java.util.UUID;
 
+import com.chgvcode.y.users.config.Role;
+
 public record RegisterUserResponse(
     UUID uuid,
     String username,
     String firstName,
     String lastName,
+    Role role,
     Instant createdAt
 ) {}
--- src/main/java/com/chgvcode/y/users/dto/UserResponse.java
+++ src/main/java/com/chgvcode/y/users/dto/UserResponse.java
@@ -3,8 +3,11 @@ package com.chgvcode.y.users.dto;
 import java.time.Instant;
 import java.util.UUID;
 
+import com.chgvcode.y.users.config.Role;
+
 public record UserResponse(
     UUID uuid,
     String username,
+    Role role,
     Instant createdAt
 ) {}
